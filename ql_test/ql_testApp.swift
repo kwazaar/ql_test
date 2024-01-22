6f26531aeffc65e58b0dcea81f24eede2e9b796f
kwazaar <maksim_sizov@icloud.com>
2024-01-22 22:31:28 +0300

    Initial Commit

---
+++ ql_test/ql_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  ql_testApp.swift
+//  ql_test
+//
+//  Created by Максим Сизов on 22.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ql_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
